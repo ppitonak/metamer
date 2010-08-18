0bb525e85e99274771c6f815438600d2e68a13ac
ppitonak@redhat.com <ppitonak@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2010-08-18 12:40:00 +0000

    * added 2 new pages for rich:inplaceInput * added rich:inplaceInput to rich:dataTable, rich:extendedDataTable, h:dataTable and rich:subTable
    git-svn-id: http://anonsvn.jboss.org/repos/richfaces/modules/tests/metamer/trunk@18752 46473bb9-a92a-0410-8f14-e4f2f95f0492

--- application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
+++ application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
@@ -114,6 +114,7 @@ public class RichBean implements Serializable {
         components.put("richDataTable", "Rich Data Table");
         components.put("richExtendedDataTable", "Rich Extended Data Table");
         components.put("richFunctions", "Rich Functions");
+        components.put("richInplaceInput", "Rich Inplace Input");
         components.put("richJQuery", "Rich jQuery");
         components.put("richList", "Rich List");
         components.put("richPanel", "Rich Panel");
