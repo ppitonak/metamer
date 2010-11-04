423aca7f86dcc19aa44b83ce45433aab3a9e4147
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2010-11-04 13:14:03 +0000

    rich:tooltip - added two samples: with default targetting to parent component and with custom target (RF-9589)
    git-svn-id: http://anonsvn.jboss.org/repos/richfaces/modules/tests/metamer/trunk@19929 46473bb9-a92a-0410-8f14-e4f2f95f0492

--- application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
+++ application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
@@ -141,6 +141,7 @@ public class RichBean implements Serializable {
         components.put("richToggleControl", "Rich Toggle Control");
         components.put("richTogglePanel", "Rich Toggle Panel");
         components.put("richTogglePanelItem", "Rich Toggle Panel Item");
+        components.put("richTooltip", "Rich Tooltip");
         components.put("richTree", "Rich Tree");
     }
 
