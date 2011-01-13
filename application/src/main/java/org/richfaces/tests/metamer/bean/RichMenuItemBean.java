8fa9560aaca034d6406f6e016da4c1702a0fafe7
ppitonak@redhat.com <ppitonak@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2011-01-13 13:22:44 +0000

    https://issues.jboss.org/browse/RFPL-958
    * added 30 tests for rich:menuItem
    
    
    git-svn-id: http://anonsvn.jboss.org/repos/richfaces/modules/tests/metamer/trunk@20982 46473bb9-a92a-0410-8f14-e4f2f95f0492

--- application/src/main/java/org/richfaces/tests/metamer/bean/RichMenuItemBean.java
+++ application/src/main/java/org/richfaces/tests/metamer/bean/RichMenuItemBean.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat, Inc. and individual contributors
+ * Copyright 2010-2011, Red Hat, Inc. and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -58,6 +58,9 @@ public class RichMenuItemBean implements Serializable {
         attributes.setAttribute("label", "New");
         attributes.setAttribute("mode", "ajax");
         attributes.setAttribute("rendered", true);
+
+        attributes.remove("action");
+        attributes.remove("actionListener");
     }
 
     public Attributes getAttributes() {
