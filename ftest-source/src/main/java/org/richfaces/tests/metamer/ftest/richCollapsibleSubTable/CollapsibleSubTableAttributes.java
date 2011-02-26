3cf7edc6af252787d2220a28dc2064e309620e57
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2011-02-26 15:30:38 +0000

    changed way of checking show data facet checkbox working in both - Mojarra and MyFaces
    git-svn-id: http://anonsvn.jboss.org/repos/richfaces/modules/tests/metamer/trunk@21954 46473bb9-a92a-0410-8f14-e4f2f95f0492

--- ftest-source/src/main/java/org/richfaces/tests/metamer/ftest/richCollapsibleSubTable/CollapsibleSubTableAttributes.java
+++ ftest-source/src/main/java/org/richfaces/tests/metamer/ftest/richCollapsibleSubTable/CollapsibleSubTableAttributes.java
@@ -21,13 +21,14 @@
  *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richCollapsibleSubTable;
 
-import static org.jboss.test.selenium.guard.request.RequestTypeGuardFactory.guardXhr;
 import static org.richfaces.tests.metamer.ftest.AbstractMetamerTest.pjq;
 
 import java.util.Collection;
 
+import org.jboss.test.selenium.GuardRequest;
 import org.jboss.test.selenium.dom.Event;
 import org.jboss.test.selenium.locator.JQueryLocator;
+import org.jboss.test.selenium.request.RequestType;
 import org.richfaces.ExpandMode;
 import org.richfaces.model.SortMode;
 import org.richfaces.tests.metamer.ftest.AbstractComponentAttributes;
@@ -40,9 +41,14 @@ public class CollapsibleSubTableAttributes extends AbstractComponentAttributes {
 
     JQueryLocator showDataLocator = pjq("input[id$=noDataCheckbox]");
 
-    public void setShowData(boolean showData) {
-        selenium.check(showDataLocator, showData);
-        guardXhr(selenium).fireEvent(showDataLocator, Event.CLICK);
+    public void setShowData(final boolean showData) {
+        new GuardRequest(RequestType.XHR) {
+            public void command() {
+                selenium.check(showDataLocator, showData);
+                selenium.fireEvent(showDataLocator, Event.CLICK);
+                selenium.fireEvent(showDataLocator, Event.CHANGE);
+            }
+        }.waitRequest();
     }
 
     public void setExpandMode(ExpandMode expandMode) {
